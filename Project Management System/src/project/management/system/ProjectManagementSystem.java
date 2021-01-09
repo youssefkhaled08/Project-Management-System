df25729fefd8072967051371e25790398b51f402
youssefkhaled08 <ykfhe08@gmail.com>
2021-01-10 00:23:22 +0200

    Initialized Project.

---
+++ Project Management System/src/project/management/system/ProjectManagementSystem.java
@@ -0,0 +1,8 @@
+package project.management.system;
+
+public class ProjectManagementSystem {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }    
+}
